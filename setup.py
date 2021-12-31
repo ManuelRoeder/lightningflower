131da6ec3ac11475adf5a4dbd1cffdfa152fba5e
ManuelRoeder <mroeder57@gmail.com>
2021-12-31 23:08:15 +0100

    lf: Add setup tools
    Add project dependencies.
    Set baseline release to 0.1.0
    
    Type: New Feature

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 
+required_packages = [
+    "torch", "flwr", "pytorch-lightning", "numpy"
+]
+
 setup(
-    name='lightningflower',
-    version='0.1.0',
-    description='Flower based Pytorch lightning framework',
-    url='https://github.com/ManuelRoeder/lightningflower',
-    author='Manuel Roeder',
-    author_email='manuel.roeder@fhws.de',
-    license='MIT',
-    packages=['lightningflower'],
-    install_requires=['flwr>=0.17.0',
-                      'pytorch-lightning>=1.5.4',
-                      'numpy>=1.21.4',
-                      ],
+    name="lightningflower",
+    version="0.1.0",
+    description="Flower based Pytorch lightning framework",
+    url="https://github.com/ManuelRoeder/lightningflower",
+    author="Manuel Roeder",
+    author_email="manuel.roeder@web.de",
+    license="MIT",
+    packages=["lightningflower"],
+    install_requires=required_packages,
+    python_requires='>=3.8.12',
+    package_data={"": ["README.md", "LICENSE"]},
 )
