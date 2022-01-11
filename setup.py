7455397868f70ae56280516ef79a1c43ecd2afd0
ManuelRoeder <mroeder57@gmail.com>
2022-01-11 09:46:06 +0100

    lf: Update setup file
    Update setup file to include long
    description

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 required_packages = [
     "torch", "flwr", "pytorch-lightning", "numpy"
 ]
 
 setup(
     name="lightningflower",
-    version="0.1.1",
+    version="0.1.2",
     description="Flower based Pytorch lightning framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ManuelRoeder/lightningflower",
     author="Manuel Roeder",
     author_email="manuel.roeder@web.de",
